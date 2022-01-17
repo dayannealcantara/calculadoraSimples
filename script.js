76d42e0751a595e0f23b3190b1ccd64bf1bfcb78
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-01-17 16:48:55 -0300

    operações e igualdade

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
+const display = document.getElementById("display");
+const numeros = document.querySelectorAll("[id*=tecla]");
+const operadores = document.querySelectorAll("[id*=operador]");
 
-const display = document.getElementById('display');
-const numeros = document.querySelectorAll('[id*=tecla]');
+let novoNumero = true;
+let operador;
+let numeroAnterior;
 
-const atualizarDisplay = (texto) =>{
-    display: display.textContent += texto;
+const operacaoPendente = () => operador !== undefined;
+
+const calcular = () => {
+    if(operacaoPendente()){
+        const numeroAtual = parseFloat(display.textContent);
+        novoNumero = true;
+        if (operador == '+'){
+            atualizarDisplay(numeroAnterior + numeroAtual)
+        } else if (operador == '-') {
+            atualizarDisplay(numeroAnterior - numeroAtual)
+        }else if (operador == '*') {
+            atualizarDisplay(numeroAnterior * numeroAtual)
+        }else if (operador == '/') {
+            atualizarDisplay(numeroAnterior / numeroAtual)
+        }
+    }
 }
 
-const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+const atualizarDisplay = (texto) => {
+  if (novoNumero) {
+    display.textContent = texto;
+    novoNumero = false;
+  } else {
+    display.textContent += texto;
+  }
+};
 
-numeros.forEach(numero => numero.addEventListener('click', inserirNumero));
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+numeros.forEach(numero => numero.addEventListener("click", inserirNumero));
+
+const selecionarOperador = (evento) => {
+  if (!novoNumero) {
+    calcular();
+    novoNumero = true;
+    operador = evento.target.textContent;
+    numeroAnterior = parseFloat(display.textContent);
+    
+  }
+};
+
+operadores.forEach(operador => operador.addEventListener("click", selecionarOperador));
+const ativarIgual = () => {
+    calcular();
+    operador = undefined
+}
+document.getElementById('igual').addEventListener('click', ativarIgual)
+
